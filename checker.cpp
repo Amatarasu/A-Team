2672aaab20f840985770c9875d209e6cff3fce3a
jensenj5 <jensenj5@gator.uhd.edu>
2016-03-19 21:09:36 -0500

    checker

--- checker.cpp
+++ checker.cpp
@@ -1,70 +1,74 @@
-int checker(gameBoard cell, int x, int y, int player, int score){
-
-	int col=row=diag=diag2=1; //initiated to 1 since there is always the one just placed
-
-	//column
-	for(int i = 1; i < 4; i++){
-		if (cell[x][y+i] == player)
-			col++;
-		else 
-			break;
-	}
-	for(int i = 1; i < 4; i++){
-		if (cell[x][y-i] == player)
-			col++
-		else
-			break;
-	}
-	if col >=4
-		score += col - 3;
-	
-	//row
-	for(int i = 1; i < 4){
-		if cell[x+i][y] == player;
-			row++;
-		else 
-			break;
-	}
-	for(int i = 1; i < 4){
-		if cell[x-i][y] == player;
-			row++;
-		else 
-			break;
-	}
-	if row >= 4
-		score += row - 3;
-	
-	//diag '\'
-	for(int i = 1; i < 4){
-		if cell[x+i][y+i] == player;
-			diag++;
-		else 
-			break;
-	}
-	for(int i = 1; i < 4){
-		if cell[x-i][y-i] == player;
-			diag++;
-		else 
-			break;
-	}
-	if diag >= 4
-		score += row - 3;
-	
-	//diag2 /
-	for(int i = 1; i < 4){
-		if cell[x+i][y-i] == player;
-			diag2++;
-		else 
-			break;
-	}
-	for(int i = 1; i < 4){
-		if cell[x-i][y+i] == player;
-			diag2++;
-		else 
-			break;
-	}
-	if diag2 >= 4
-		score += row - 3;
+int checker(int cell[6][6], int x, int y, int player, int score){
+ 
+    int col = 1;
+	int row = 1;
+	int diag = 1;
+	int diag2 = 1; //initiated to 1 since there is always the one just placed
+ 
+    //column
+    for(int i = 1; i < 4; i++){
+        if (cell[x][y+i] == player)
+            col++;
+        else
+            break;
+    }
+    for(int i = 1; i < 4; i++){
+        if (cell[x][y-i] == player)
+            col++;
+        else
+            break;
+    }
+    if (col >=4)
+        score += (col - 3);
+     
+    //row
+    for(int i = 1; i < 4; i++){
+        if (cell[x+i][y] == player)
+            row++;
+        else
+            break;
+    }
+    for(int i = 1; i < 4; i++){
+        if (cell[x-i][y] == player)
+            row++;
+        else
+            break;
+    }
+    if (row >= 4)
+        score += (row - 3);
+     
+    //diag '\'
+    for(int i = 1; i < 4; i++){
+        if (cell[x+i][y+i] == player)
+            diag++;
+        else
+            break;
+    }
+    for(int i = 1; i < 4; i++){
+        if (cell[x-i][y-i] == player)
+            diag++;
+        else
+            break;
+    }
+    if (diag >= 4)
+        score += (diag - 3);
+     
+    //diag2 /
+    for(int i = 1; i < 4; i++){
+        if (cell[x+i][y-i] == player)
+            diag2++;
+        else
+            break;
+    }
+    for(int i = 1; i < 4; i++){
+        if (cell[x-i][y+i] == player)
+            diag2++;
+        else
+            break;
+    }
+    if (diag2 >= 4)
+        score += (diag2 - 3);
+ 	cout << player << " " << score << endl;
 
 return score;
 }
