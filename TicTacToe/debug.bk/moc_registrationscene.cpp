45a518edeeca40df6b82d0710cc3d4f763066f6a
jensenj5 <jensenj5@gator.uhd.edu>
2016-03-19 19:28:55 -0500

    update

---
+++ TicTacToe/debug.bk/moc_registrationscene.cpp
@@ -0,0 +1,116 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'registrationscene.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../registrationscene.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'registrationscene.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_registrationScene_t {
+    QByteArrayData data[4];
+    char stringdata0[77];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_registrationScene_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_registrationScene_t qt_meta_stringdata_registrationScene = {
+    {
+QT_MOC_LITERAL(0, 0, 17), // "registrationScene"
+QT_MOC_LITERAL(1, 18, 23), // "on_signupButton_clicked"
+QT_MOC_LITERAL(2, 42, 0), // ""
+QT_MOC_LITERAL(3, 43, 33) // "on_registrationHelpButton_cli..."
+
+    },
+    "registrationScene\0on_signupButton_clicked\0"
+    "\0on_registrationHelpButton_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_registrationScene[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x08 /* Private */,
+       3,    0,   25,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void registrationScene::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        registrationScene *_t = static_cast<registrationScene *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_signupButton_clicked(); break;
+        case 1: _t->on_registrationHelpButton_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject registrationScene::staticMetaObject = {
+    { &QDialog::staticMetaObject, qt_meta_stringdata_registrationScene.data,
+      qt_meta_data_registrationScene,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *registrationScene::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *registrationScene::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_registrationScene.stringdata0))
+        return static_cast<void*>(const_cast< registrationScene*>(this));
+    return QDialog::qt_metacast(_clname);
+}
+
+int registrationScene::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
--- checker.cpp
+++ checker.cpp
@@ -1,67 +1,70 @@
-coor = [x,y] //coordinates being played
-player //?? maybe bool, int or char depends on how we pass it in
+int checker(gameBoard cell, int x, int y, int player, int score){
 
-int col=row=diag=diag2=1; //initiated to 1 since there is always the one just placed
-count // also passed in
+	int col=row=diag=diag2=1; //initiated to 1 since there is always the one just placed
 
-//column
-for(int i = 1; i < 4; i++){
-	if (cell[x,y+i] == player)
-		col++;
-	else 
-		break;
-}
-for(int i = 1; i < 4; i++){
-	if (cell[x,y-i] == player)
-		col++
-	else
-		break;
-}
-if col >=4
-	count += col - 3;
+	//column
+	for(int i = 1; i < 4; i++){
+		if (cell[x][y+i] == player)
+			col++;
+		else 
+			break;
+	}
+	for(int i = 1; i < 4; i++){
+		if (cell[x][y-i] == player)
+			col++
+		else
+			break;
+	}
+	if col >=4
+		score += col - 3;
+	
+	//row
+	for(int i = 1; i < 4){
+		if cell[x+i][y] == player;
+			row++;
+		else 
+			break;
+	}
+	for(int i = 1; i < 4){
+		if cell[x-i][y] == player;
+			row++;
+		else 
+			break;
+	}
+	if row >= 4
+		score += row - 3;
+	
+	//diag '\'
+	for(int i = 1; i < 4){
+		if cell[x+i][y+i] == player;
+			diag++;
+		else 
+			break;
+	}
+	for(int i = 1; i < 4){
+		if cell[x-i][y-i] == player;
+			diag++;
+		else 
+			break;
+	}
+	if diag >= 4
+		score += row - 3;
+	
+	//diag2 /
+	for(int i = 1; i < 4){
+		if cell[x+i][y-i] == player;
+			diag2++;
+		else 
+			break;
+	}
+	for(int i = 1; i < 4){
+		if cell[x-i][y+i] == player;
+			diag2++;
+		else 
+			break;
+	}
+	if diag2 >= 4
+		score += row - 3;
 
-//row
-for(int i = 1; i < 4){
-	if cell[x+i, y] == player;
-		row++;
-	else 
-		break;
+return score;
 }
-for(int i = 1; i < 4){
-	if cell[x-i, y] == player;
-		row++;
-	else 
-		break;
-}
-if row >= 4
-	count += row - 3;
-//diag \
-for(int i = 1; i < 4){
-	if cell[x+i, y+i] == player;
-		diag++;
-	else 
-		break;
-}
-for(int i = 1; i < 4){
-	if cell[x-i, y-i] == player;
-		diag++;
-	else 
-		break;
-}
-if diag >= 4
-	count += row - 3;
-//diag2 /
-for(int i = 1; i < 4){
-	if cell[x+i, y-i] == player;
-		diag2++;
-	else 
-		break;
-}
-for(int i = 1; i < 4){
-	if cell[x-i, y+i] == player;
-		diag2++;
-	else 
-		break;
-}
-if diag2 >= 4
-	count += row - 3;
