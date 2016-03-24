c0e7d4a36007ef8dda0c3032eae4f0566c7af9ca
[ibra] <[cisse_ibra@yahoo.fr]>
2016-03-24 15:16:19 -0500

    added more statement towards gaem mode and also few icons as well

--- TicTacToe/gameoption.cpp
+++ TicTacToe/gameoption.cpp
@@ -31,6 +31,14 @@ void gameOption::on_gameOptionOkay_clicked()
         playerVsAi->gameStart();
         close();
     }
+    else if(ui->choiceToGoFirst->currentIndex() == 2)
+    {
+        QMessageBox :: setInformativeText("this has been selected! 2");
+    }
+    else if(ui->choiceToGoFirst->currentIndex() == 3)
+    {
+        QMessageBox :: setInformativeText("this has been selected! 2");
+    }
     else
     {
         QMessageBox::information(this,tr("GameMode"),tr("Please select a Game Mode!"));
